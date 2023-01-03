055a344279190e5673e2bceebd5d2677fe4c2ea7
Shivam Daksh <shivamdakshshivam@gmail.com>
2023-01-03 14:27:11 +0530

    Add files via upload

--- bmi/shivam.py
+++ bmi/shivam.py
@@ -1,17 +1,21 @@
 while True:
+    print("")
     wt = float(input('Enter you weight (in Kg) : '))
     ht = float(input('Enter you weight (in cm/m/feet) : '))
     if (300>ht>100):
         ht = ht/100
     elif(3.28084<ht<9.84252):
-        ht = ht*30.48
+        ht = (ht*30.48)/100
     else:
         ht = ht
     bmi = wt/(ht**2)
-    print("Your bmi is ", bmi)
+    print("")
+    print("Hey! Your bmi is ", bmi)
+    print("")
     if bmi<18:
         print('Your bmi is too low!')
     elif 18<=bmi<=25:
         print("What's up! fit person")
-    else
+    else:
         print("Please start excercising")
+    print("")
