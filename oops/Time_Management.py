3303f22db78581301482734245149fba622727f0
Shivam Daksh <shivamdakshshivam@gmail.com>
2023-04-07 20:20:47 +0530

    Add files via upload

--- oops/Time_Management.py
+++ oops/Time_Management.py
@@ -1,25 +1,32 @@
-class person:
-    def __init__(self, fname, lname, age, bgroup,e_type, p_id:
-    fname=input("Input your first name: ")
-    lname= input('Enter your last name: ')
-    age = int(input("Enter you age: "))
-    bggroup = input("Enter your blood group: ")
-    e_type = input("Enter your profession: ")
-    profession= input("What is your profession? : ")
-    self.firstName = fname
-    self.lastName = lname
-    self.age = age
-    self.bgroup = bggroup
-    self.e_type = e_type
-    self.profession = profession
-    self.person_id = p_id
-class faculty(person):
-    def __init__(self, subject, dept, lec):
-        sub = input("Enter your subject")
-        dept = input("Enter you department")
-        lec = int(input("Enter the no. of lectures : "))
-        self.subject = sub
-        self.department = dept
-        self.lectures = lec
-            
-        
+class Person:
+    def __init__(self):
+        self.first_name = input("Enter your first name: ")
+        self.last_name = input("Enter your last name: ")
+        self.blood_group = input("Blood Group: ")
+        self.age = int(input('Age: '))
+
+
+class Faculty(Person):
+    def __init__(self):
+        super().__init__()
+        self.subject = input('Subject: ')
+        self.department = input('Department: ')
+        self.lectures = int(input('No. of Lectures: '))
+
+
+class Student(Person):
+    def __init__(self):
+        super().__init__()
+        self.course = input('Course/Program: ')
+        self.branch = input('Branch: ')
+        self.spln = input('Specialisation: ')
+        self.section = input('Section: ')
+class Class:
+    def __init__(self):
+        self.branch = input('Branch: ')
+        self.section = input('Section: ')
+        self.strength = input('Strength: ')
+        self.lectures = input('No. of Lectures: ')
+        self.building = input('Building: ')
+        self.floor = int(input('Floor: '))
+        self.room_no = input("Room No.: ")
